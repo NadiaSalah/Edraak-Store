2e30666b677aa1cba649c1530574e71e97e2cd22
Nadia Salah <43372566+NadiaSalah@users.noreply.github.com>
2022-12-19 08:30:05 +0200

    updateAppServiceProvider

--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,7 +1,7 @@
 <link href="{{ asset('assets/css/normalize.css') }}" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
 <link href="{{ asset('assets/fontawesome/css/all.min.css')}}" rel="stylesheet">
 <link href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
 
